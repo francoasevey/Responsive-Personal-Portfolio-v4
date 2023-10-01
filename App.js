3dfaa6463bf572167b8f3f94dcca71b4cea27d12
francoromeo <francoromeo5012@gmail.com>
2023-10-01 19:10:35 -0300

    form

--- App.js
+++ App.js
@@ -34,3 +34,22 @@ var mixer = mixitup(containerEl, {
 });
 
 mixer.filter('*');
+
+const swiper = new Swiper('.swiper', {
+    
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+
+    breakpoints: {
+        600: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+})
