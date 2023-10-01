d36dac47551d1fa63366269be9fd59e691ef796c
francoromeo <francoromeo5012@gmail.com>
2023-09-30 21:49:01 -0300

    services

--- App.js
+++ App.js
@@ -1,1 +1,27 @@
-App.js
+const servicesButtons = document.querySelectorAll('.service_item');
+const servicesDetails = document.querySelector('.services_right');
+
+const getService = (category) => {
+    const details = servicesData.find(item => item.category === category);
+    servicesDetails.innerHTML = `
+    <h3>${details.title}</h3>
+    ${details.description.map(paragraph => "<p>" + paragraph + "</p>").join('')}
+    `
+}
+
+const removeActiveClass = () =>{
+    servicesButtons.forEach(button => {
+        button.classList.remove('active')
+    })
+}
+
+servicesButtons.forEach(item => {
+    item.addEventListener('click', () => {
+        removeActiveClass()
+        const serviceClass = item.classList[1];
+        getService(serviceClass)
+        item.classList.add('active')
+    })
+})
+
+getService('frontend')
---
+++ data.js
@@ -0,0 +1,17 @@
+const servicesData = [
+    {
+        title: "Frontend Depeloper",
+        category: "frontend",
+        description: ['Frontend Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae repellat quidem tempore praesentium. A sequi inventore dolor repellat? Qui amet odio repellendus eaque doloribus, rerum mollitia omnis excepturi.', 'Dolores aspernatur magnam vitae impedit deserunt? Inventore molestias consectetur similique aliquam repellat iusto at maiores, cumque vel harum velit nisi ab natus eos id amet asperiores magnam deserunt neque voluptates! Odit, exercitationem', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae repellat quidem tempore praesentium. A sequi inventore dolor repellat? Qui amet odio repellendus eaque doloribus.']
+    },
+    {
+        title: "Backend Depeloper",
+        category: "backend",
+        description: ['Backend Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae repellat quidem tempore praesentium. A sequi inventore dolor repellat? Qui amet odio repellendus eaque doloribus, rerum mollitia omnis excepturi.', 'Dolores aspernatur magnam vitae impedit deserunt? Inventore molestias consectetur similique aliquam repellat iusto at maiores, cumque vel harum velit nisi ab natus eos id amet asperiores magnam deserunt neque voluptates! Odit, exercitationem', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae repellat quidem tempore praesentium. A sequi inventore dolor repellat? Qui amet odio repellendus eaque doloribus.']
+    },
+    {
+        title: "Fullstack Development",
+        category: "fullstack",
+        description: ['Fullstack Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae repellat quidem tempore praesentium. A sequi inventore dolor repellat? Qui amet odio repellendus eaque doloribus, rerum mollitia omnis excepturi.', 'Dolores aspernatur magnam vitae impedit deserunt? Inventore molestias consectetur similique aliquam repellat iusto at maiores, cumque vel harum velit nisi ab natus eos id amet asperiores magnam deserunt neque voluptates! Odit, exercitationem', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae repellat quidem tempore praesentium. A sequi inventore dolor repellat? Qui amet odio repellendus eaque doloribus.']
+    }
+]
