ace426ed9a1882833c3a65bc30f5b14aade30731
francoromeo <francoromeo5012@gmail.com>
2023-10-02 14:37:13 -0300

    toggle js

--- App.js
+++ App.js
@@ -53,3 +53,29 @@ const swiper = new Swiper('.swiper', {
         }
     }
 })
+
+const navMenu = document.querySelector('.nav_menu');
+const navOpenBtn = document.querySelector('.nav_toggle-open');
+const navCloseBtn = document.querySelector('.nav_toggle-close');
+
+const openNavHandler = () => {
+    navMenu.style.display = 'flex';
+    navOpenBtn.style.display = 'none';
+    navCloseBtn.style.display = 'inline-block';
+}
+
+const closeNavHandler = () => {
+    navMenu.style.display = 'none';
+    navOpenBtn.style.display = 'inline-block';
+    navCloseBtn.style.display = 'none';
+}
+
+navOpenBtn.addEventListener('click', openNavHandler);
+navCloseBtn.addEventListener('click', closeNavHandler);
+
+const navItems = navMenu.querySelectorAll('a');
+if(window.innerWidth < 768) {
+    navItems.forEach(item => {
+        item.addEventListener('click', closeNavHandler)
+    })
+}
